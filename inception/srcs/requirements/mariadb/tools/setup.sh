79b91771874d5c1b617263d708a4a8d82e7efb62
Delyassss <ilyassda04@gmail.com>
2026-05-10 23:00:47 +0100

    start

---
+++ inception/srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,14 @@
+mysqld_safe --datadir=/var/lib/mysql & 
+while ! mysqladmin ping --silent ; do
+sleep 1
+done
+mysql -u root << EOF
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+mysqladmin shutdown 
+
+mysqld_safe --datadir=/var/lib/mysql
