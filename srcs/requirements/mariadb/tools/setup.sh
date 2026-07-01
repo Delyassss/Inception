01f9686fae7effdb7fa9cd718f5ddf0243a490d4
Delyassss <ilyassda04@gmail.com>
2026-07-01 15:25:27 +0100

    nariadbbbbb

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -11,28 +11,34 @@ then
 MYSQL_PASSWORD=$(cat /run/secrets/MYSQL_PASSWORD)
 MYSQL_ROOT_PASSWORD=$(cat /run/secrets/MYSQL_ROOT_PASSWORD)
 
-echo "launching mariadb in the background"
+if [ -z "$MYSQL_PASSWORD" ]; then
+    echo "ERROR: MYSQL_PASSWORD variable is empty!"  >> /home/output.log
+    exit 1
+fi
+
+
+echo "launching mariadb in the background"  >> /home/output.log
   
-mysqld_safe --datadir=/var/lib/mysql & # mysqld_safe is a wrapper script that starts the mysqld server and monitors it, restarting it if it crashes. It also provides some additional features, such as logging and error handling.
+mysqld_safe --datadir=/var/lib/mysql --syslog=0 --log-error=/dev/stdou & # mysqld_safe is a wrapper script that starts the mysqld server and monitors it, restarting it if it crashes. It also provides some additional features, such as logging and error handling.
 
 while ! mysqladmin ping --silent ; do # while repeats only when the command returns success (exit code 0) 
-echo "mariadb not ready yet..."
+echo "mariadb not ready yet..."  >> /home/output.log
 sleep 1
 done
 
-echo "Creating the Database"
+echo "Creating the Database" >> /home/output.log
 
 mysql -u root  << EOF
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_NAME}\`;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-ALTER USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED VIA mysql_native_password BY '${MYSQL_PASSWORD}';
+ALTER USER '${MYSQL_USER}'@'%' IDENTIFIED VIA mysql_native_password BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_NAME}\`.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-echo "Done Creating the Database"
+echo "Done Creating the Database"  >> /home/output.log
 
 mysqladmin shutdown  # mysqld by default exit after Injecting so we launch at the end in the backgrounds (free port 3306 )
 fi 
 
-exec mysqld_safe --datadir=/var/lib/mysql # exec is used so the script replaces itself with the main process (mysqld_safe)
+exec mysqld_safe --datadir=/var/lib/mysql  # exec is used so the script replaces itself with the main process (mysqld_safe)
