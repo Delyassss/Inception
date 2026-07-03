c8a2e2f2a7dd7522d97cee198d6e04fda7fa1c1d
Delyassss <ilyassda04@gmail.com>
2026-07-03 21:25:56 +0100

    For the ftp service we need to set the perm just for the READ (vsftpd Quit  if the conf is writeable by everyone)

--- srcs/requirements/ftp/tools/setup.sh
+++ srcs/requirements/ftp/tools/setup.sh
@@ -5,11 +5,22 @@
 
 FTP_PASSWORD=$(cat /run/secrets/FTP_PASSWORD)
 
+if [ ! -f /etc/ssl/certs/vsftpd.crt ]
+	then
+	openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/ssl/private/vsftpd.key \
+    -out /etc/ssl/certs/vsftpd.crt \
+    -subj "/C=MA/ST=BeniMellal/L=Khouribga/O=1337/CN=idabouno.42.fr"
+fi
 
 # -d: Set their home exactly to the shared volume
 # -s: Block terminal access for security
 # -u 33: Forge the identity to match the web server
-# -,m smfds
+
+if id "${FTP_USER}" > /dev/null 2>&1;
+then
+	echo "USER ${FTP_USER} already exists"
+else
 
 useradd -m "${FTP_USER}"
 
@@ -18,9 +29,14 @@ echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
 
 usermod -aG www-data "${FTP_USER}"
+echo $FTP_USER >> /etc/vsftpd.user_list # allow this user to connect
 
+fi
+echo "Starting ..."
+mkdir -p /var/run/vsftpd/empty # vsftpd reates a temporary clone of itself (a worker process) to talk to the visitor but strips all root priv the clone process is located at that path
 
-
-exec vsftpd "/etc/vsftpd.conf"
+chmod 600 /etc/vsftpd.conf
+# the cloned process only job is to handle the net traffic and authentication
+exec vsftpd /etc/vsftpd.conf
 
 
